aebc6890842147a6925a0be65f18a9d6caf7be67
Avinash Prasad <avi062@gmail.com>
2020-04-25 09:49:19 +0530

    Init project and added seperate Oauth files as per revisions on flow. | [Next] => Create identity server to handle request

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Claim.DoB")]
+        public IActionResult SecretPolicy()
+        {
+            return View("Secret");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult SecretRole()
+        {
+            return View("Secret");
+        }
+
+        [AllowAnonymous]
+        public IActionResult Authenticate()
+        {
+            //Claims
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "Bob@fmail.com"),
+                new Claim(ClaimTypes.DateOfBirth, "11/11/1111"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim("Grandma.Says", "Very nice boi."),
+            };
+            var licenceClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob K Foo"),
+                new Claim("DrivingLicence", "A+"),
+            };
+
+            //Identity
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenceIdentity = new ClaimsIdentity(licenceClaims, "Government");
+
+            //Claims Principle
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenceIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DoStuff(
+            [FromServices] IAuthorizationService authorizationService)
+        {
+            // we are doing stuff here
+
+            var authResult = await authorizationService.AuthorizeAsync(User, "Claim.DoB");
+
+            if (authResult.Succeeded)
+            {
+                return View("Index");
+            }
+
+            return View("Index");
+        }
+    }
+}
