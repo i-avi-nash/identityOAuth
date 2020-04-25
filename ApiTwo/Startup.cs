af55a0e96fd5a08f14bed88d4a83b769edb6a118
Avinash Prasad <avi062@gmail.com>
2020-04-26 00:26:22 +0530

    Create Identity server project and configured | Added Two api and a client to get the flow moving | got token

---
+++ ApiTwo/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiTwo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication()
+                .AddJwtBearer("Bearer", config =>
+                {
+                    config.Authority = "https://localhost:44372/";
+
+                    config.Audience = "ApiTwo";
+                });
+
+            services.AddHttpClient();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
